cfa81fd93507633ac473b692523b1fca1bd6efcb
Mateo Monsalve <79293866+mateo-mons@users.noreply.github.com>
2024-01-05 18:21:51 +0000

    Little changes

--- pages/3_DataFrame_Demo.py
+++ pages/3_DataFrame_Demo.py
@@ -1,17 +1,3 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from urllib.error import URLError
 
 import altair as alt
@@ -31,7 +17,7 @@ def data_frame_demo():
     try:
         df = get_UN_data()
         countries = st.multiselect(
-            "Choose countries", list(df.index), ["China", "United States of America"]
+            "Choose countries", list(df.index), ["Colombia", "United States of America"]
         )
         if not countries:
             st.error("Please select at least one country.")
