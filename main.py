636117fc27b4535ebad85fef9c35719a4b6e51d5
Mateo Monsalve <79293866+mateo-mons@users.noreply.github.com>
2024-01-18 18:37:17 +0000

    Refining things

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import streamlit as st 
 import PyPDF2 # Importa la biblioteca para trabajar con archivos pdf
-import streamlit as st
-from streamlit.logger import get_logger
 
 
-LOGGER = get_logger(__name__)
+st.set_page_config(
+    page_title="Merge PDFs!!!",
+    page_icon="🧲",
+)
+
 
 # -- VARIABLES -- #
 
-output_pdf = "documents/final_pdf.pdf" # Define el nombre del archivo de salida que se guardará en pdf final
+output_pdf = "documents/final_pdf.pdf" # Define  la ruta y el nombre del archivo de salida que se guardará en pdf final
 
 # -- FUNCTIONS -- #
 
@@ -21,35 +24,25 @@ def merge_pdfs(output_path, documents):
     final_pdf.write(output_path) # Guarda el PDF combinado en la ruta de salida
 
 
-def run():
-    st.set_page_config(
-        page_title="Merge PDFs!!!",
-        page_icon="🧲",
-    )
 
-    # -- FRONT -- #
-    st.header('Merge PDFs!')
-    st.image('assets/pdf_merge.jpeg')
-    st.subheader('Attach PDFs')
-    attached_pdfs = st.file_uploader(label="🕸", accept_multiple_files=True)
+# -- FRONT -- #
+st.header('Merge PDFs!')
+st.image('assets/pdf_merge.jpeg')
+st.subheader('Attach PDFs')
+attached_pdfs = st.file_uploader(label="🕸", accept_multiple_files=True)
 
-    merge = st.button(label="Merge 🕸") # Crea un boton para unir los pdfs #
+merge = st.button(label="Merge 🕸") # Crea un boton para unir los pdfs #
 
 
-    # -- BACK -- #
-
-    if merge:
-        if len(attached_pdfs) == 0:
-            st.warning('Attach PDFs!')
-        elif len(attached_pdfs) <= 1:
-            st.warning('Attach more than one PDF!')
-        else:
-            merge_pdfs(output_pdf, attached_pdfs)
-            st.success('Now you can download your PDF!')
-            with open(output_pdf, 'rb') as file:
-                pdf_data = file.read()
-            st.download_button(label='Download final PDF🔭', data=pdf_data, file_name='final_pdf.pdf')
-
-
-if __name__ == "__main__":
-    run()
+# -- BACK -- #
+if merge:
+    if len(attached_pdfs) == 0:
+        st.warning('Attach PDFs!')
+    elif len(attached_pdfs) <= 1:
+        st.warning('Attach more than one PDF!')
+    else:
+        merge_pdfs(output_pdf, attached_pdfs)
+        st.success('Now you can download your PDF!')
+        with open(output_pdf, 'rb') as file:
+            pdf_data = file.read()
+        st.download_button(label='Download final PDF🔭', data=pdf_data, file_name='final_pdf.pdf')
